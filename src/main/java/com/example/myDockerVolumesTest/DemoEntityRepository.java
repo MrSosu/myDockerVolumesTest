68d187b759382aa0dbbe9ec2b92143794614cbfc
m_adriani <m.adriani@jdk.it>
2024-06-26 09:41:40 +0200

    start

--- src/main/java/com/example/myDockerVolumesTest/DemoEntityRepository.java
+++ src/main/java/com/example/myDockerVolumesTest/DemoEntityRepository.java
@@ -1,6 +1,8 @@
 package com.example.myDockerVolumesTest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DemoEntityRepository extends JpaRepository<DemoEntity, Long> {
 }
--- src/test/java/com/example/myDockerVolumesTest/MyDockerVolumesTestApplicationTests.java
+++ src/test/java/com/example/myDockerVolumesTest/MyDockerVolumesTestApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.myDockerVolumesTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class MyDockerVolumesTestApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
